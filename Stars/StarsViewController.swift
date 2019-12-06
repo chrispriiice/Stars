7675bce57037ebd2f2c0e8219fbae80972fb9867
Christopher Price <christopher.r.price@gmail.com>
2019-12-05 22:08:56 -0500

    Set up views and outlets.

---
+++ Stars/StarsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Chris Price on 12/5/19.
+//  Copyright © 2019 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+    
+    // Mark: - IBOutlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // Mark: - IBActions
+    @IBAction func printStars(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createStar(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
